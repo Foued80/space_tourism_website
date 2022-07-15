51588aa26a119ef27032523b499c43027b790169
Foued80 <foued23dz@gmail.com>
2022-07-15 05:38:20 +0200

    commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: isProd ? "https://foued80.github.io/space_tourism_website" : "",
   reactStrictMode: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
